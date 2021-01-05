bb2dd9bc3109fe5db31821316fd375753a1edf7c
tim1207 <timlin891207@gmail.com>
2021-01-05 16:54:52 +0800

    first commit

---
+++ DS HW code/BFS&DFS.py
@@ -0,0 +1,40 @@
+graph ={
+    "A":["B","C"],
+    "B":["A","C","D"],
+    "C":["A","B","D","E"],
+    "D":["B","C","E","F"],
+    "E":["C","D"],
+    "F":["D"]
+}
+
+def BFS(graph,s):
+    queue=[]
+    queue.append(s)
+    seen=set()
+    seen.add(s)
+    while(len(queue)>0):
+        vertex=queue.pop(0)
+        nodes=graph[vertex]
+        for w in nodes:
+            if w not in seen:
+                queue.append(w)
+                seen.add(w)
+        print(vertex)
+
+def DFS(graph,s):
+    stack=[]
+    stack.append(s)
+    seen=set()
+    seen.add(s)
+    while(len(stack)>0):
+        vertex=stack.pop()
+        nodes=graph[vertex]
+        for w in nodes:
+            if w not in seen:
+                stack.append(w)
+                seen.add(w)
+        print(vertex)
+
+# BFS(graph,"A")
+
+DFS(graph,"A")
